ef02fc654b027e4160d6ed6af7150b05eef2af6b
Susanne König <susanne.koenig@powercoders.org>
2022-04-26 10:03:36 +0200

    practice JS by logging object properties

---
+++ week_5/scripts.js
@@ -0,0 +1,23 @@
+const book = {
+    title: "The prisoner of Azkaban",
+    author: "J. K. Rowling",
+    published: 1999,
+    numberOfPages: 448,
+    rating: 5,
+    bestseller: true,
+    series: "Harry Potter",
+    volume: 3,
+    ISBN: "0439136350",
+    publisher: "Scholastic",
+    bookmarkedPage: 325,
+    loadBookmarkedPage: function(){
+        return this.bookmarkedPage;
+    }
+};
+
+console.log(`"${book.title}" by ${book.author}`);
+
+book.title = "Harry Potter and the prisoner of Azkaban";
+console.log(`"${book.title}" by ${book.author}`);
+
+console.log(book.loadBookmarkedPage());
