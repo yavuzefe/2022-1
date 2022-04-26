708123d4d456a0dc4d5664c57bc5742c4ed7e822
Susanne König <susanne.koenig@powercoders.org>
2022-04-26 16:57:02 +0200

    change array to object and loop thourgh that

--- week_5/scripts.js
+++ week_5/scripts.js
@@ -161,10 +161,41 @@ favoriteAnimals.forEach(function(animal,index){
 favoriteAnimals.forEach((animal,index) => {
     console.log(`My #${index+1} favorite animal is ${animal}.`);
 });
-*/
 
-let favoriteAnimals = ['monkey','lion','panda','cat','falcon'];
+
 for(let i = 0; i < favoriteAnimals.length; i++){
     console.log(`My #${i+1} favorite animal is ${favoriteAnimals[i]}.`);
 }
+let i = 0;
+while(i < favoriteAnimals.length){
+    console.log(`My #${i+1} favorite animal is ${favoriteAnimals[i]}.`);
+    i++;
+}
 
+let favoriteAnimals = ['monkey','lion','panda','cat','falcon'];
+let i = 1;
+for (const animal of favoriteAnimals) {
+    console.log(`My #${i} favorite animal is ${animal}.`);
+    i++;
+}
+*/
+
+const presidents = {
+    names: ['Putin','Trump'],
+    countries: ['Russia','USA']
+}
+
+let presidentList = [
+    {
+        lname: 'Putin',
+        country: 'Russia'
+    },
+    {
+        lname: 'Biden',
+        country: 'USA'
+    }
+]
+
+presidentList.forEach(president => {
+    console.log(`The president of ${president.country} is ${president.lname}.`)
+})
