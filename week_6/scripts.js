533bcc48328f19c673eee41a8491e1a32107e5e1
Susanne König <susanne.koenig@powercoders.org>
2022-05-02 08:57:55 +0200

    prepare exercise for class

---
+++ week_6/scripts.js
@@ -0,0 +1,9 @@
+/** declare list of authors based on img names */
+
+/** create object per author */
+
+/** create function createTiles to create masonry tiles */
+
+/** create function addTiles to add all tiles to the DOM randomly */
+
+/** add a eventListener on the logo to rebuild the list on click */
